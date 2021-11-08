fe00e3edb9de35681e759837ad879ae51d459a8b
DataThink <xia.he88@gmail.com>
2021-11-08 10:15:09 +0100

    uploaded Pdfs not to be saved in app.py

--- app/app.py
+++ app/app.py
@@ -1,5 +1,9 @@
 import os
 from io import StringIO
+from pathlib import Path
+from re import T
+from tempfile import NamedTemporaryFile
+
 from pdfminer.converter import TextConverter
 from pdfminer.layout import LAParams
 from pdfminer.pdfdocument import PDFDocument
@@ -8,7 +12,6 @@ from pdfminer.pdfpage import PDFPage
 from pdfminer.pdfparser import PDFParser
 
 import spacy
-# import joblib
 import shutil
 from fastapi import FastAPI, File, UploadFile
 from fastapi import Request, Response
@@ -17,7 +20,6 @@ from fastapi.templating import Jinja2Templates
 from pydantic import BaseModel
 import uvicorn 
 
-# crf_model = joblib.load("/model/nerCRF.joblib") 
 
 app = FastAPI(
     title="{{HortiSem.Named_Entity_Recognition}}",
@@ -27,12 +29,13 @@ app = FastAPI(
 
 class Data(BaseModel):
     text: str
+    
 # app.mount("/static", StaticFiles(directory="static"), name="static")
 # templates = Jinja2Templates(directory="templates")
 
 nlp = spacy.load(os.getcwd()+"/model/spacy_ml_rule_model")
 
-UPLOAD_FOLDER = os.getcwd()+"/upload_folder"
+# UPLOAD_FOLDER = os.getcwd()+"/upload_folder"
 
 def read_pdf(file_path):
     output_string = StringIO()
@@ -63,12 +66,23 @@ def preprocess_pdf(string):
         string_final += line
     return string_final
 
-# def predict(text, nlp_model):
-#     doc = nlp_model(text)
-#     ents = []
-#     for ent in doc.ents:
-#         ents.append({"entity":ent.text, "label":ent.label_})    
-#     return {"ents":ents}
+def save_upload_file(upload_file: UploadFile, destination: Path) -> None:
+    try:
+        with destination.open("wb") as buffer:
+            shutil.copyfileobj(upload_file.file, buffer)
+    finally:
+        upload_file.file.close()
+
+
+def save_upload_file_tmp(upload_file: UploadFile) -> Path:
+    try:
+        suffix = Path(upload_file.filename).suffix
+        with NamedTemporaryFile(delete=False, suffix=suffix) as tmp:
+            shutil.copyfileobj(upload_file.file, tmp)
+            tmp_path = Path(tmp.name)
+    finally:
+        upload_file.file.close()
+    return tmp_path
 
 def predict(text, nlp_model):
     doc = nlp_model(text)
@@ -89,21 +103,14 @@ def predict(text, nlp_model):
 
 @app.post("/predict_pdf/")
 async def create_upload_file(file: UploadFile = File(...)):  
-    # the user can upload a pdf file or direct give text . 
-    file_object = file.file
-    
-    #create empty file to copy the file_object to
-    Pdf_path = os.path.join(UPLOAD_FOLDER, file.filename)
-    
-    print(Pdf_path)
-    # save the pdf file
-    upload_folder = open(Pdf_path, 'wb+')
-    shutil.copyfileobj(file_object, upload_folder)
-    upload_folder.close()
-    # Process the text
-    input_text = read_pdf(Pdf_path)
-    # Predict
-    ents = predict(input_text,nlp)
+    tmp_path = save_upload_file_tmp(file)
+    try:
+        # Process the text
+        input_text = read_pdf(tmp_path)
+        # Predict
+        ents = predict(input_text,nlp)
+    finally:
+        tmp_path.unlink() # delete the temp file
     
     return {"filename": file.filename, "text":input_text,"ents":ents}
 
